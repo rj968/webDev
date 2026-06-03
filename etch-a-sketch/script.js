8cc428714cabd965a9ac853c264bd7e375cd55ff
rj968 <rishi.g.jain968@gmail.com>
2026-06-03 19:16:00 +0530

    Refactor code. Add clear and change side buttons.
    Improve readability. Add helper functions and restructure code.
    Add clear and change grid buttons which don't do anything rn.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,28 +1,72 @@
-let side = 16
-const grid = document.querySelector(".grid");
-let grid_size = 1000;
-let box_size = grid_size / side;
+const GRID_SIZE = 1000; // size of grid in pixels
+const MAX_SIDE = 100;
 
-for(let i = 0; i < side; i++)
+const gridEl = document.querySelector(".grid");
+const changeGridEl = document.querySelector(".change-grid")
+const clearGridEl = document.querySelector(".clear-grid")
+
+
+function createGrid(side)
 {
-    let row = document.createElement("div")
-    row.classList.add("row");
-    for(let j = 0; j < side; j++)
+    let box_size = GRID_SIZE / side;
+
+    for(let i = 0; i < side; i++)
     {
-        let box = document.createElement("div")
-        box.classList.add("box")
-        box.style.height = box_size + "px";
-        box.style.width = box_size + "px";
-        row.appendChild(box)
+        let row = document.createElement("div")
+        row.classList.add("row");
+        for(let j = 0; j < side; j++)
+        {
+            let box = document.createElement("div")
+            box.classList.add("box")
+            box.style.height = box_size + "px";
+            box.style.width = box_size + "px";
+            row.appendChild(box)
+        }
+        gridEl.appendChild(row);
     }
-    grid.appendChild(row)
 }
 
-grid.addEventListener("mouseover", 
-    (event) => {
-        if(!event.target.classList.contains("box"))
-            return;
-        event.target.style.backgroundColor = "red";
-    }
-)
+function colorGridMouse()
+{
+    gridEl.addEventListener("mouseover", 
+        (event) => {
+            if(!event.target.classList.contains("box"))
+                return;
+            event.target.style.backgroundColor = "red";
+        }
+    )
+}
 
+function isIntegerInput(input) 
+{
+  // Eliminate empty strings or whitespace-only strings which coerce to 0
+  if (typeof input === 'string' && input.trim() === '') {
+    return false;
+  }
+  // Check if the forced number conversion is finite
+  return Number.isInteger(Number(input));
+}
+
+function getSideLength()
+{    
+    let side = window.prompt("Enter the number of boxes in each row (Max = 100): ");
+    if(!isIntegerInput(side))
+        return alert("Invalid input. Try again.");
+    side = Number(side);
+    if(side <= 0 || side > MAX_SIDE)
+        return alert("Invalid input. Try again.")
+    return side
+}
+
+let side = 16;
+createGrid(side);
+colorGridMouse();
+
+console.log(changeGridEl)
+changeGridEl.onClick = function() {
+    console.log('Button clicked via onclick property.');
+};
+
+clearGridEl.onClick = function() {
+    console.log('Button clicked via onclick property.');
+};
