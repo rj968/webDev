e598da8de89450a59a2eb6b616aa1d35e7d3144d
rj968 <rishi.g.jain968@gmail.com>
2026-06-07 03:51:40 +0530

    Add keyboard compatability and backspace feature

--- basic-calculator/script.js
+++ basic-calculator/script.js
@@ -37,24 +37,31 @@ function addToOutput(str)
     outputEl.textContent += str;
 }
 
+function backspace()
+{
+    outputEl.textContent =
+        outputEl.textContent.slice(0, -1);
+}
 
-function getInput()
+
+function getMouseInput()
 {
     containerEl.addEventListener("click", event => 
     {
+        if(!event.target.matches("button"))
+            return;
 
-        if(outputEl.textContent.charAt(0).match(/[a-zA-Z0]/))
+        if(outputEl.textContent.charAt(0).match(/[a-zA-Z]/))
             clear();
 
         if(event.target.id == "equals")
-        {
             return outputEl.textContent += "\n" + calculate(outputEl.textContent.split(""));
-        }
+
+        if(event.target.id == "backspace")
+            return backspace();
 
         if(event.target.id == "clear")
-        {
             return clear();
-        }
 
         if(event.target.classList.contains("symbol") || 
             event.target.classList.contains("number") ||
@@ -63,6 +70,37 @@ function getInput()
             return addToOutput(event.target.textContent);
         }
     })
+}
+
+function getKeyboardInput()
+{
+    document.addEventListener("keydown", event=> 
+    {
+        if(outputEl.textContent.charAt(0).match(/[a-zA-Z0]/))
+            clear();
+
+        if(event.key === "Enter")
+            return outputEl.textContent += "\n" + calculate(outputEl.textContent.split(""));
+
+        if(event.ctrlKey && event.key === "Backspace")
+            return clear();
+
+        if(event.key === "Backspace")
+            return backspace();
+
+
+        if(event.key.match(/[0-9+\-*/.]/))
+        {
+            return addToOutput(event.key)
+        }
+    }
+    )
+}
+
+function getInput()
+{
+    getKeyboardInput();
+    getMouseInput();
 }
 
 function isSymbol(char)
